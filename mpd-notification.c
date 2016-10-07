1d81f87dd158974354f0e29f9c84e088d311c2af
Christian Hesse <mail@eworm.de>
2016-10-07 14:44:31 +0200

    sort and complete help output

--- mpd-notification.c
+++ mpd-notification.c
@@ -259,7 +259,7 @@ int main(int argc, char ** argv) {
 		printf("%s: %s v%s (compiled: " __DATE__ ", " __TIME__ ")\n", program, PROGNAME, VERSION);
 
 	if (help > 0)
-		fprintf(stderr, "usage: %s [-h] [-H HOST] [-p PORT] [-m MUSIC-DIR] [-s PIXELS] [-t TIMEOUT] [-v] [-V]\n", program);
+		fprintf(stderr, "usage: %s [-h] [-H HOST] [-m MUSIC-DIR] [-o] [-p PORT] [-s PIXELS] [-t TIMEOUT] [-v] [-V]\n", program);
 
 	if (version > 0 || help > 0)
 		return EXIT_SUCCESS;
