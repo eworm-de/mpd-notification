eworm-de/mpd-notification

commit 06d4b9b7a160ee1fbdc419f16dd6b8c9277288a8

update for ffmpeg 4.x
The function av_register_all() is deprecated since commit
0694d8702421e7aff1340038559c438b61bb30dd.

mpd-notification.c
@@ -338,7 +338,9 @@ int main(int argc, char ** argv) {
 
 #ifdef HAVE_LIBAV
 	/* libav */
+#if LIBAVCODEC_VERSION_INT < AV_VERSION_INT(58, 9, 100)
 	av_register_all();
+#endif
 
 	/* only fatal messages from libav */
 	if (verbose == 0)

commit 298832026b747af68f1a277cc26f1c3ecaa8a5f9

fix libav version check
Checking with LIBAVCODEC_VERSION_INT is stupid if we do not include
libavcodec... Let's use LIBAVFORMAT_VERSION_INT instead.
This broke with commit 06d4b9b7a160ee1fbdc419f16dd6b8c9277288a8.

Fixes #42

mpd-notification.c
@@ -353,7 +353,7 @@ int main(int argc, char ** argv) {
 
 #ifdef HAVE_LIBAV
 	/* libav */
-#if LIBAVCODEC_VERSION_INT < AV_VERSION_INT(58, 9, 100)
+#if LIBAVFORMAT_VERSION_INT < AV_VERSION_INT(58, 9, 100)
 	av_register_all();
 #endif
 